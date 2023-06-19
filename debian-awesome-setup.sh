309f2cf6094c89e806dafdcef8bbe87160c8ab15
Ertan Kayalar <ertankayalar@gmail.com>
2023-06-19 19:33:01 +0300

    flatpak

--- debian-awesome-setup.sh
+++ debian-awesome-setup.sh
@@ -1,7 +1,28 @@
 #!/usr/bin/env bash
+#==============================================================================================================
+#
+#          FILE:  debian-awesome-setup.sh
+#
+#         USAGE:  bash debian-awesome-setup.sh
+#
+#   DESCRIPTION:  Installation AwesomeWM on a clean Debian Bookworm (12)
+#
+#        STATUS:  TESTED
+#
+#       OPTIONS:  ---
+#  REQUIREMENTS:  a clean Debian Bookworm (12) 
+#                 installed with git 
+#          BUGS:  ---
+#         NOTES:  Read README.md
+#        AUTHOR:  Ertan Kayalar, ertankayalar@gmail.com
+#       COMPANY:  private
+#       VERSION:  0.01
+#       CREATED:  19-06-2023
+#      REVISION:  ---
+#===============================================================================================================
 
-# Debian Awesome WM Setup
 sudo apt update 
+
 
 # zram
 sudo apt install -y zram-tools vim micro
@@ -22,10 +43,16 @@ sudo apt install -y xfce4 xfce4-goodies samba
 sudo apt install -y awesome awesome-extra luarocks lightdm
 
 # system tools
-sudo apt install -y git neofetch htop vim xfce4-power-manager numlockx volumeicon-alsa nitrogen variety chromium feh imagemagick rofi i3lock bluez bluez-alsa-utils bluez-tools blueman scrot alsa-utils xbacklight conky-all redshift cmatrix picom
+sudo apt install -y git ufw neofetch htop vim xfce4-power-manager numlockx volumeicon-alsa  rofi i3lock bluez bluez-alsa-utils bluez-tools blueman scrot alsa-utils xbacklight conky-all redshift cmatrix picom 
+
+# apps
+sudo apt install -y chromium 
+
+# image tools
+sudo apt install -y nitrogen variety feh imagemagick flameshot
 
 # utils
-sudo apt install -y g810-led unrar unzip p7zip-full flameshot
+sudo apt install -y g810-led unrar unzip p7zip-full 
 
 # python 
 sudo apt install -y python3-pip python3-neovim
@@ -40,6 +67,10 @@ sudo apt install -y alacritty kitty xsel yank
 sudo apt install -y fonts-font-awesome fonts-ubuntu fonts-liberation2 fonts-liberation fonts-terminus 
 
 # Install Nerd Fonts
- source ./nerdfonts.sh
+source nerdfonts.sh
 
+# Flatpak
+sudo apt install flatpak -y
+# apt install gnome-software-plugin-flatpak -y
 
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
