43d74d622ebd706fbbc5ebf05ef77e52400919ce
Ertan Kayalar <ertan@ertankayalar.com>
2023-06-19 16:49:44 +0300

    brew-setup

--- brew-setup.sh
+++ brew-setup.sh
@@ -16,8 +16,13 @@ brew install neovim fzf exa ripgrep starship lsd tmux
 
 brew install bat zoxide lsd
 
-brew install nodejs npm
+brew install nodejs npm 
+npm i -g eslint neovim npm-check-updates
 
+# install from original source
+curl -fsSL https://get.pnpm.io/install.sh | sh -
+
+pnpm i -g neovim eslint npm-check-updates
 
 
 
