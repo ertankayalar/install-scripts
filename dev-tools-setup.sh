a1ab6f9dc7ea15b512abf7e0b54f059c55b18a1d
Ertan Kayalar <ertankayalar@gmail.com>
2023-06-20 16:37:43 +0300

    dev-tools-setup

---
+++ dev-tools-setup.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+#==============================================================================================================
+#
+#          FILE:  dev-tools-setup.sh
+#
+#         USAGE:  bash dev-tools-setup.sh
+#
+#   DESCRIPTION:  Installation of dev tools
+#
+#        STATUS:  TESTED
+#
+#       OPTIONS:  ---
+#  REQUIREMENTS:  a clean Debian Bookworm (12) 
+#                 installed with git 
+#          BUGS:  ---
+#         NOTES:  Read README.md
+#        AUTHOR:  Ertan Kayalar, ertankayalar@gmail.com
+#       COMPANY:  private
+#       VERSION:  0.01
+#       CREATED:  19-06-2023
+#      REVISION:  ---
+#===============================================================================================================
+# homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> $HOME/.profile
+(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> $HOME/.bashrc
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+# brew tools
+
+brew install gcc 
+
+#tmuxp 
+brew install neovim fzf exa ripgrep starship lsd tmux 
+
+brew install bat zoxide lsd
+
+brew install nodejs npm 
+npm i -g eslint neovim npm-check-updates
+
+# install from original source
+curl -fsSL https://get.pnpm.io/install.sh | sh -
+
+pnpm i -g neovim eslint npm-check-updates
+
+
+
