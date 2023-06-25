8782bd5d7bc6becaad76e974c4c54d3a834b3218
Ertan Kayalar <ertankayalar@gmail.com>
2023-06-25 14:44:01 +0300

    gtk-theme

--- gtk-theme-install.sh
+++ gtk-theme-install.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+THEME_FOLDER = "~/Documents/Linux/Themes"
+mkdir ~/Documents/Linux
+cd $THEME_FOLDER 
 
 # Fedora
 # dnf install gtk-murrine-engine gtk2-engines
@@ -6,18 +10,31 @@
 # debian & ubuntu
 sudo apt-get install -y gtk2-engines-murrine gtk2-engines-pixbuf sassc
 
-mkdir ~/Documents/Linux
-mkdir ~/Documents/Linux/Themes 
-cd ~/Documents/Linux/Themes
-
+# Qogir Gtk Theme
+cd $THEME_FOLDER
 git clone https://github.com/vinceliuice/Qogir-theme.git 
 cd Qogir-theme
-
 ./install.sh -i debian --tweaks square
 
-
-cd ~/Documents/Linux/Themes
+# Qogir Icon Theme
+cd $THEME_FOLDER
 git clone https://github.com/vinceliuice/Qogir-icon-theme.git
 cd Qogir-icon-theme
 ./install.sh 
+
+# Colloid Gtk Theme 
+cd $THEME_FOLDER
+git clone https://github.com/vinceliuice/Colloid-icon-theme.git
+cd Colloid-icon-theme 
+./install.sh --scheme dracula --theme purple teal
+
+# Lavanda GTK Theme
+cd $THEME_FOLDER
+git clone https://github.com/vinceliuice/Lavanda-gtk-theme.git
+cd Lavanda-gtk-theme
+./install.sh --icon debian
+
+
+cd ~/
+printf "\e[1;32m Themes are installed.\e[0m\n"
 
