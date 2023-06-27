abf6fb70cd33fa7559addf6c5f3eef3ba5daba9d
Ertan Kayalar <ertankayalar@gmail.com>
2023-06-27 11:20:01 +0300

    new-dotfiles

--- 30-config.sh
+++ 30-config.sh
@@ -8,13 +8,20 @@ chmod +x ~/bin/*
 sudo cp ~/bin/pulseaudio-control.bash /bin/
 \cp -r ~/Downloads/dotfiles/.config/backgrounds/ ~/.config/
 \cp -r ~/Downloads/dotfiles/.config/bspwm/ ~/.config/
+\cp -r ~/Downloads/dotfiles/.config/dk/ ~/.config/
 \cp -r ~/Downloads/dotfiles/.config/dunst/ ~/.config/
 \cp -r ~/Downloads/dotfiles/.config/i3/ ~/.config/
 \cp -r ~/Downloads/dotfiles/.config/picom/ ~/.config/
 \cp -r ~/Downloads/dotfiles/.config/polybar/ ~/.config/
 \cp -r ~/Downloads/dotfiles/.config/qtile/ ~/.config/
 \cp -r ~/Downloads/dotfiles/.config/sxhkd/ ~/.config/
+\cp -r ~/Downloads/dotfiles/.config/suckless/ ~/.config/
+\cp -r ~/Downloads/dotfiles/.config/tint2/ ~/.config/
 \cp -r ~/Downloads/dotfiles/.config/rofi/ ~/.config/
-
+\cp -r ~/Downloads/dotfiles/.config/neofetch/ ~/.config/
+\cp -r ~/Downloads/dotfiles/.config/redshift/ ~/.config/
+\cp -r ~/Downloads/dotfiles/.config/alacritty/ ~/.config/
+\cp -r ~/Downloads/dotfiles/.config/kitty/ ~/.config/
+\cp -r ~/Downloads/dotfiles/.config/tmux/ ~/.config/
 
 
--- 40-dev-install.sh
+++ 40-dev-install.sh
@@ -1,31 +1,42 @@
 #!/usr/bin/env bash
 
 
-# Nodejs 20
-curl -fsSL https://deb.nodesource.com/setup_20.x | bash - &&\
-apt-get install -y nodejs
-
 # Neovim
 # Build prerequisites
-sudo apt install ninja-build gettext cmake unzip curl
+sudo apt install -y ninja-build gettext cmake unzip curl
 
 # clone
 git clone https://github.com/neovim/neovim
 cd neovim
 git checkout stable
 make CMAKE_BUILD_TYPE=RelWithDebInfo
-cd build && cpack -G DEB && sudo dpkg -i nvim-linux64.deb
-
+cd build && cpack -G DEB && sudo dpkg -i -y nvim-linux64.deb
 
 # lazy.nvim
-
  
-sudo apt install fzf exa ripgrep lsd bat
+sudo apt install -y fzf exa ripgrep lsd bat zoxide
 
 # Starship
 curl -sS https://starship.rs/install.sh | sh
 
+# Nodejs 
+sudo -i
+curl -fsSL https://deb.nodesource.com/setup_20.x | bash - &&\
+apt-get install -y nodejs
+exit 
+
 # PNPM Installation 
 curl -fsSL https://get.pnpm.io/install.sh | sh -
 
-# zoxide
+# npm global setup
+rm -rf $HOME/.npm-global
+mkdir $HOME/.npm-global
+npm config set prefix $HOME/.npm-global
+echo 'export PATH="$HOME/.npm-global/bin:$PATH"' | tee -a ~/.profile 
+source $HOME/.profile
+
+
+CONFIG_HOME="$HOME/.config"
+
+git clone https://github.com/tmux-plugins/tpm "$CONFIG_HOME/tmux/plugins/tpm"
+"$CONFIG_HOME/tmux/plugins/tpm/bin/install_plugins"
