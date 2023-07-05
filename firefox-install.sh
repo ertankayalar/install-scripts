1aa49960cf7b8bc95e317b0dcefba13e4f8b3439
Ertan Kayalar <ertan@ertankayalar.com>
2023-07-05 10:02:41 +0300

    firefox-install-beta

---
+++ firefox-install.sh
@@ -0,0 +1,9 @@
+
+
+
+cd ~/Downloads
+sudo tar xvjf firefox-98.0.1.tar.bz2 -C /opt
+sudo nano /usr/share/applications/firefox-stable.desktop
+
+
+sudo ln -s /opt/firefox/firefox /usr/local/bin/firefox
