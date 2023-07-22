b889edaf6f5bed8be4c530d615b8db9080157c2e
Ertan Kayalar <ertan@ertankayalar.com>
2023-07-22 09:49:58 +0300

    go-install

---
+++ go-install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+cd ~/Downloads
+
+wget https://golang.org/dl/go1.20.6.linux-amd64.tar.gz
+
+# remove previous install
+sudo rm -rf /usr/local/go 
+
+# install go
+sudo tar -C /usr/local -xzf go1.20.2.linux-amd64.tar.gz
+
+echo "export PATH=/usr/local/go/bin:${PATH}" | sudo tee -a $HOME/.bashrc
+
+source $HOME/.bashrc
+
+go version
