27f95f4d55b929d376e61f36d945816478084b08
Ertan Kayalar <ertan@ertankayalar.com>
2023-07-25 16:02:16 +0300

    docker-install

--- docker-install.sh
+++ docker-install.sh
@@ -13,7 +13,7 @@ echo \
 
 # Install Docker
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Verify
 sudo docker run hello-world
