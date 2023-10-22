06b7b20b3f9b3b92b2318ca4e08e516bcda23acf
Ertan Kayalar <ertan@ertankayalar.com>
2023-10-22 10:25:04 +0300

    mint-go-ffox

---
+++ mint/install-virt.sh
@@ -0,0 +1,4 @@
+sudo apt install qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils -y
+sudo apt install virt-manager -y
+
+# sudo usermod -G libvirt -a usr_name
---
+++ mint/starship-install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+#
+# Starship
+curl -sS https://starship.rs/install.sh | sh
