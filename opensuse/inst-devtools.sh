aaa2ca0f03fd1071afe17ab6cf75fc5222ab685c
Ertan Kayalar <ertan@ertankayalar.com>
2024-02-27 08:39:58 +0300

    opensuse

---
+++ opensuse/inst-devtools.sh
@@ -0,0 +1,6 @@
+ 
+ # luarocks
+sudo zypper install -y  alacritty kitty xsel yank tmux htop sqlite3 whois fzf eza ripgrep lsd bat zoxide neovim hugo
+sudo zypper install -y nodejs npm yarn
+
+sudo zypper install -y starship
---
+++ opensuse/inst-flatpak-apps.sh
@@ -0,0 +1,16 @@
+
+
+# use telegram from suse repo
+#flatpak install flathub org.telegram.desktop -y
+#flatpak install flathub com.brave.Browser -y
+sudo flatpak install flathub com.spotify.Client -y
+#flatpak install flathub md.obsidian.Obsidian -y 
+sudo flatpak install flathub com.vivaldi.Vivaldi -y
+sudo flatpak install flathub com.discordapp.Discord -y
+# flatpak install flathub com.microsoft.Edge -y
+sudo flatpak install flathub com.bitwarden.desktop -y
+sudo flatpak install flathub com.anydesk.Anydesk -y
+#flatpak install flathub org.kde.kdenlive -y
+sudo flatpak install flathub us.zoom.Zoom -y
+
+
