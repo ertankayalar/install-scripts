0cf2722967a2b0cd58a8ab473de36dc5dfef9a02
Ertan Kayalar <ertankayalar@gmail.com>
2024-03-02 13:47:57 +0300

    debian

--- debian/update-g810-profile.sh
+++ debian/update-g810-profile.sh
@@ -1,7 +1,8 @@
 # g810 led profile updater
-sudo cp usr/share/doc/g810-led/examples/sample_profiles/all_green /etc/g810-led/profile 
-sudo cp usr/share/doc/g810-led/examples/sample_profiles/fx_color_green /etc/g810-led/profile 
-sudo cp usr/share/doc/g810-led/examples/sample_profiles/colors /etc/g810-led/profile 
-sudo cp usr/share/doc/g810-led/examples/sample_profiles/group_keys /etc/g810-led/profile 
-g810-led -p /etc/g810-led/profile
+#sudo cp usr/share/doc/g810-led/examples/sample_profiles/all_green /etc/g810-led/profile
+#sudo cp usr/share/doc/g810-led/examples/sample_profiles/fx_color_green /etc/g810-led/profile
+#sudo cp usr/share/doc/g810-led/examples/sample_profiles/colors /etc/g810-led/profile
+#sudo cp usr/share/doc/g810-led/examples/sample_profiles/group_keys /etc/g810-led/profile
+sudo cp $HOME/OS/install-scripts/g810/g810_all_teal /etc/g810-led/profile
+sudo g810-led -p /etc/g810-led/profile
 
