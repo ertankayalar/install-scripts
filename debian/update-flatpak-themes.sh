de64fd9608d0a54115137a85ac405c797e49496b
Ertan Kayalar <ertankayalar@gmail.com>
2024-03-02 07:19:53 +0300

    flatpak-debian

--- debian/update-flatpak-themes.sh
+++ debian/update-flatpak-themes.sh
@@ -1,2 +1,6 @@
+# update flatpak themes and icons
+#
+# https://itsfoss.com/flatpak-app-apply-theme/
+#
 sudo flatpak override --filesystem=$HOME/.themes
 sudo flatpak override --filesystem=$HOME/.local/share/icons 
