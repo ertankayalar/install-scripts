1357ad76cd81530f406990fca41c8b1d4fad2325
Ertan Kayalar <ertankayalar@gmail.com>
2024-03-28 15:56:56 +0300

    ruby-nvidia-go

--- debian/go-install.sh
+++ debian/go-install.sh
@@ -2,17 +2,17 @@
 
 cd ~/Downloads
 
-wget https://golang.org/dl/go1.22.0.linux-amd64.tar.gz
+wget https://golang.org/dl/go1.22.1.linux-amd64.tar.gz
 
 # remove previous install
 sudo rm -rf /usr/local/go 
 
 # install go
-sudo tar -C /usr/local -xzf go1.22.0.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.22.1.linux-amd64.tar.gz
 
 
 echo 'export PATH="/usr/local/go/bin:$PATH"' | tee -a ~/.bashrc
 
-source $HOME/.bashrc
+source ~/.bashrc
 
 go version
---
+++ debian/ruby-install.sh
@@ -0,0 +1,13 @@
+#/bin/bash 
+# asdf 
+git clone https://github.com/excid3/asdf.git ~/.asdf
+echo '. "$HOME/.asdf/asdf.sh"' >> ~/.bashrc
+echo '. "$HOME/.asdf/completions/asdf.bash"' >> ~/.bashrc
+echo 'legacy_version_file = yes' >> ~/.asdfrc
+echo 'export EDITOR="nvim --wait"' >> ~/.bashrc
+exec $SHELL
+
+
+# Debian dependencies
+sudo apt-get install autoconf patch build-essential rustc libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libgmp-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev uuid-dev
+
