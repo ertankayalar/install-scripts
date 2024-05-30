81e36950dc81913ebd689660fa8c5aa16641a491
Ertan Kayalar <ertankayalar@gmail.com>
2024-05-30 16:05:50 +0300

    dev-tools_snap-apps_22

--- ubuntu/devtools-24.sh
+++ ubuntu/devtools-24.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
+# devtools 24.04
 sudo apt update
 
 sudo apt install -y git curl
 sudo apt install -y kitty xsel yank tmux htop whois fzf ripgrep lsd zoxide eza bat 
-sudo apt install -y python3-pip sqlite3 luarocks neovim python3-pynvim nodejs npm yarnpkg
+sudo apt install -y python3-pip sqlite3 luarocks python3-pynvim nodejs npm yarnpkg
+
+# neovim python3-pynvim nodejs npm yarnpkg
 
 
--- ubuntu/snap-apps.sh
+++ ubuntu/snap-apps.sh
@@ -1,11 +1,13 @@
  
 # brave hugo telegram  spotify
 #
-sudo snap install spotify
+#sudo snap install spotify
 sudo snap install telegram-desktop
 sudo snap install code --classic
 sudo snap install hugo
-sudo snap install brave
+# sudo snap install brave
 sudo snap install bitwarden
 sudo snap install typora
-# sudo snap install nvim --classic
+sudo snap install nvim --classic
+sudo snap install alacritty --classic
+sudo snap install lsd
