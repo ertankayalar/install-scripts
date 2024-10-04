9ed2482e6403095edd4cd265b1cac1bff2aa80e6
Ertan Kayalar <ertankayalar@gmail.com>
2024-10-04 09:52:40 +0300

    flatpak-gnome

--- ubuntu/install-flatpak.sh
+++ ubuntu/install-flatpak.sh
@@ -5,5 +5,7 @@ sudo apt -y install flatpak
 # Add flathub repository
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
+sudo apt install -y --install-suggests gnome-software
+
 # Install gnome-software-plugin-flatpak
 sudo apt install -y gnome-software-plugin-flatpak
